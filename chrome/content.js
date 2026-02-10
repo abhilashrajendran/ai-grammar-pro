410947d22e2d3fe3a68714d14b1d671438b4dacf
Abhilash <meetabhilash@gmail.com>
2026-02-09 20:19:22 -0600

    feat: Implement the core content script for grammar checking, text manipulation, and floating button UI in editable fields.

--- chrome/content.js
+++ chrome/content.js
@@ -283,10 +283,11 @@ function createFloatingButton(textarea) {
     floatingButton.setAttribute('type', 'button');
     floatingButton.setAttribute('title', 'Check grammar (Ctrl+Shift+G)');
     floatingButton.innerHTML = `
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
-        </svg>
-    `;
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180" width="30" height="30">
+            <path cx="9" cy="9" r="1" fill="#588D3F" stroke="#FFFFFF" stroke-width="10" d="M175 90A85 85 0 0 1 90 175 85 85 0 0 1 5 90a85 85 0 0 1 170 0z"/>
+            <path x="3.5" y="3.5" width="11" height="11" rx="1.5" fill="none" stroke="#FFFFFF" stroke-width="10" d="M50 35h80l15 15v80a15 15 0 0 1 -15 15H50a15 15 0 0 1 -15 -15V50a15 15 0 0 1 15 -15z"/>
+            <path fill="#F4F6F9" d="m80.85 65.63 -15.22 48.75h6.32l4.56 -15.12H92.1l4.71 15.12h6.47l-15.15 -48.75zm-3.16 28.86 4.19 -13.6c0.88 -3.04 1.62 -6.36 2.28 -9.47h0.22c0.66 3.04 1.4 6.29 2.35 9.55l4.26 13.52zm35.3 -28.65a3.75 3.75 0 0 0 -3.82 3.91c0 2.17 1.54 3.83 3.75 3.83h0.07a3.66 3.66 0 0 0 3.83 -3.83c0 -2.24 -1.54 -3.91 -3.83 -3.91m-3.02 13.52h6.18v35.01h-6.18z"/>
+        </svg>`;
 
     document.body.appendChild(floatingButton);
     positionFloatingButton(textarea);
